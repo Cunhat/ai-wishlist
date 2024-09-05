d91cde7e3bf1e56eac2b4d44fab48a56dc282e45
Tiago Cunha <tiago92cunha@gmail.com>
2024-09-05 22:07:09 +0100

    Create Zod schemas from drizzle

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,25 +2,28 @@ import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { category, item, pocket, wishlist } from '../db/schema';
 
-// Schema for inserting a user - can be used to validate API requests
+// Wishlist Schemas
 export const insertWishlistSchema = createInsertSchema(wishlist);
 export type InsertWishlist = z.infer<typeof insertWishlistSchema>;
-// Schema for selecting a user - can be used to validate API responses
+
 export const selectWishlistSchema = createSelectSchema(wishlist);
 export type SelectWishlist = z.infer<typeof selectWishlistSchema>;
 
+// Category Schemas
 export const insertCategorySchema = createInsertSchema(category);
 export type InsertCategory = z.infer<typeof insertCategorySchema>;
 
 export const selectCategorySchema = createSelectSchema(category);
 export type SelectCategory = z.infer<typeof selectCategorySchema>;
 
+// Item Schemas
 export const insertItemSchema = createInsertSchema(item);
 export type InsertItem = z.infer<typeof insertItemSchema>;
 
 export const selectItemSchema = createSelectSchema(item);
 export type SelectItem = z.infer<typeof selectItemSchema>;
 
+// Pocket Schemas
 export const insertPocketSchema = createInsertSchema(pocket);
 export type InsertPocket = z.infer<typeof insertPocketSchema>;
 
