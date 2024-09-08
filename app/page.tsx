8fa3061e2872bbf2faf7b69bc2f6b1e229bcbe3a
Tiago Cunha <tiago92cunha@gmail.com>
2024-09-08 23:53:39 +0100

    Add chadcnUI and dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@ import { user } from "@/db/schema";
 import Image from "next/image";
 
 export default async function Home() {
-  const users = await db.select().from(user).all();
-
-  console.log(users);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello World
